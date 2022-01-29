576d4ed38541a71e48cccf245e9fff0b1a799b86
Julian Waller <git@julusian.co.uk>
2022-01-29 23:24:13 +0000

    chore: tidy scripts

--- find-run-id.mjs
+++ find-run-id.mjs
@@ -1,9 +1,14 @@
 // #!/usr/bin/env zx
 
+// silence logging output
+$.verbose = false
+
+// collect input info
 const moduleName = argv._[1]
 const tagName = argv._[2]
 const authToken = process.env.GITHUB_TOKEN
 
+// hardcoded repository info
 const owner = 'bitfocus'
 const repo = 'companion-module-publisher'
 
@@ -12,52 +17,51 @@ if (!moduleName || !tagName || !authToken) {
 	process.exit(1)
 }
 
-
+// track how long we have been waiting
 const startTime = Date.now()
 const maxWait = 30 * 1000
 
 const checkedRuns = new Set()
-
 let workflowRunId = null
 
 while (Date.now() < startTime + maxWait && !workflowRunId) {
-    const resp = await fetch(`https://api.github.com/repos/${owner}/${repo}/actions/runs?per_page=10`, {
+    // get list of jobs
+    const runList = await fetch(`https://api.github.com/repos/${owner}/${repo}/actions/runs?per_page=10`, {
         headers: {
             'Accept': 'application/vnd.github.v3+json',
             'Authorization': `token ${authToken}`
         }
     })
-    if (!resp.ok) {
-        console.error(`Workflow poll failed`, resp.statusText)
+    if (!runList.ok) {
+        console.error(`Workflow poll failed`, runList.statusText)
         process.exit(1)
     }
 
-    const respJson = await resp.json()
-    // console.log(respJson)
-
-    if (respJson.workflow_runs) {
-        for (const run of respJson.workflow_runs) {
+    const runListJson = await runList.json()
+    if (runListJson.workflow_runs) {
+        // iterate over the workflows
+        for (const run of runListJson.workflow_runs) {
             if (!checkedRuns.has(run.id)) {
                 const runUrl = run.jobs_url
 
-                const resp2 = await fetch(runUrl, {
+                // poll job info
+                const jobInfo = await fetch(runUrl, {
                     headers: {
                         'Accept': 'application/vnd.github.v3+json',
                         'Authorization': `token ${authToken}`
                     }
                 })
-                if (!resp2.ok) {
+                if (!jobInfo.ok) {
                     console.error(`Workflow poll failed`)
                     process.exit(1)
                 }
 
-                const resp2Json = await resp2.json()
-                // console.log(resp2Json)
+                const jobInfoJson = await jobInfo.json()
 
                 // ensure jobs have populated
-                if (resp2Json.jobs && resp2Json.jobs.length > 0) {
+                if (jobInfoJson.jobs && jobInfoJson.jobs.length > 0) {
                     // assume the first job
-                    const job = resp2Json.jobs[0]
+                    const job = jobInfoJson.jobs[0]
 
                     // wait for steps to populate
                     if (job.steps && job.steps.length >= 2) {
@@ -65,6 +69,7 @@ while (Date.now() < startTime + maxWait && !workflowRunId) {
                         const idStepName = job.steps[1].name
 
                         if (idStepName === `${moduleName}@${tagName}`) {
+                            // we found it!
                             workflowRunId = job.run_id
                             break
                         }
@@ -82,8 +87,10 @@ while (Date.now() < startTime + maxWait && !workflowRunId) {
 }
 
 if (!workflowRunId) {
-    console.error(`Failed to find workflow id`)
+    console.error(`Failed to find workflow id. Please check on the status manually at https://github.com/bitfocus/companion-module-publisher/actions`)
     process.exit(1)
 }
+
+// output the id
 console.log(`Found run id: ${workflowRunId}`)
 await fs.writeFile('./RUN_ID', workflowRunId.toString())
--- validate-and-build.mjs
+++ validate-and-build.mjs
@@ -10,6 +10,7 @@ if (!moduleName || !tagName) {
 	process.exit(1)
 }
 
+// ensure we are on a tag
 try {
 	await $`git -C module show-ref -q --verify "refs/tags/${tagName}" 2>/dev/null`
 } catch (e) {
@@ -17,14 +18,17 @@ try {
 	process.exit(1)
 }
 
+// load package.json
 const pkgJsonStr = await fs.readFile('./module/package.json')
 const pkgJson = JSON.parse(pkgJsonStr)
 
+// make sure the repo has the correct package.json
 if (pkgJson.name !== `@companion-module/${moduleName}`) {
-console.error(`Name in package does not match environment (Got "${pkgJson.name}", expected "@companion-module/${moduleName}")`)
+	console.error(`Name in package does not match environment (Got "${pkgJson.name}", expected "@companion-module/${moduleName}")`)
 	process.exit(1)
 }
 
+// make sure the package.json has the correct version
 if (!semver.eq(pkgJson.version, tagName)) {
 	console.error(`Version in package does not match tag (Got "${pkgJson.version}", expected "${tagName}")`)
 	process.exit(1)
@@ -47,11 +51,11 @@ if (pkgJson.scripts) {
 if (scriptName) {
 	console.log('Module needs building')
 	
+	// run the build
 	await $`yarn --cwd module install`
 	await $`yarn --cwd module run ${scriptName}`
 
 } else {
 	console.log('Module doesnt need building')
 }
-
 
